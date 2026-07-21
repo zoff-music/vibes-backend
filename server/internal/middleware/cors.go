e33703264037e264794e4ff8e99689f539b8819b
Kasper Rynning-Tønnesen <kasper@rynning-toennesen.no>
2026-07-21 22:38:58 +0200

    fix: isolate embedded room sessions

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -44,7 +44,7 @@ func (m *CORSMiddleware) Middleware(next http.Handler) http.Handler {
 
 		// Always set these for consistency.
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token, X-Session-Token, Cache-Control")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token, X-Session-Token, X-Zoff-Embed, Cache-Control")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Content-Type")
 		w.Header().Set("Access-Control-Max-Age", "86400")
 
