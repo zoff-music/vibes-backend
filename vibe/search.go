77e60913cde0ca64ff66025ba04c5c4ea7a75318
Kasper Rynning-Tønnesen <github@bundle.email>
2026-07-24 23:38:51 +0200

    refactor: simplify YouTube cache handler (#31)
    * refactor: simplify YouTube cache handler
    
    * refactor: simplify YouTube cache handler

--- vibe/search.go
+++ vibe/search.go
@@ -37,6 +37,28 @@ type CachedYouTubeTrack struct {
 	LikeCount       uint64 `json:"likeCount"`
 }
 
+func (s *CachedYouTubeSearch) GetMusicTracks() []MusicTrack {
+	tracks := make([]MusicTrack, 0, len(s.Tracks))
+	for index := range s.Tracks {
+		track := s.Tracks[index].MusicTrack()
+		tracks = append(tracks, track)
+	}
+
+	return tracks
+}
+
+func (s CachedYouTubeSearch) SetMusicTracks(tracks []MusicTrack) CachedYouTubeSearch {
+	s.Tracks = make([]CachedYouTubeTrack, 0, len(tracks))
+	for index := range tracks {
+		s.Tracks = append(
+			s.Tracks,
+			tracks[index].CachedYouTubeTrack(),
+		)
+	}
+
+	return s
+}
+
 func (t *MusicTrack) CachedYouTubeTrack() CachedYouTubeTrack {
 	return CachedYouTubeTrack{
 		ID:              t.ID,
